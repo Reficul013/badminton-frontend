edb2cc035928811e09f17b6173401dcd82223f05
Reficul013 <reficul761@gmail.com>
2025-09-10 21:56:03 -0400

    Add @vitejs/plugin-react and fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import App from './pages/App.jsx'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./pages/App.jsx";
+import "./styles.css";
 
-createRoot(document.getElementById('root')).render(<App />)
-
-
-
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
